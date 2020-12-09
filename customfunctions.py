c290acbbd2e4fa3b76c4e19d339cea8497f5030d
payam <pkhorram@ucsd.edu>
2020-12-08 18:13:46 -0800

    new update

--- customfunctions.py
+++ customfunctions.py
@@ -1,3 +1,10 @@
+'''
+    ECE 143 Project
+    Comprehensive Analysis on Suicide Rate around the World
+    Authors: Payam Khorramshahi, Dan Vu, Dylan Perlson, Zhengdong Gao 
+'''
+
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -394,3 +401,419 @@ def suicide_pearson_population(df):
 
     return pear
         
+
+def suicide_gdp(df, *country):
+    assert len(country) % 2 ==0
+    x = 2
+    y = int(len(country)/2)
+    w = 0
+    
+    fig, ax= plt.subplots(x,y, figsize=(10,8))
+    plt.subplots_adjust(wspace=0.4, hspace=0.4)
+    
+    for m in range(x):
+        for n in range(y):
+            
+            p1 = df[df['country'] == country[w]]
+            sui_num = []
+            gdp = []
+            for year in p1['year'].unique():
+                p2 = p1[p1['year'] == year]
+                sui_num.append(sum(p2.suicides_no))
+                gdp.append(np.average(p2['gdp']))
+
+            X = []
+            Y = []
+            for i,j in sorted(zip(gdp,sui_num)):
+                X.append(i)
+                Y.append(j)
+
+            ax[m,n].scatter(X,Y)
+            ax[m,n].set_title(str(country[w]))
+            
+            
+            
+            w+=1
+    for ax in ax.flat:
+        ax.set(xlabel='GDP', ylabel='Suicide Count')
+        
+    
+    plt.show()
+
+
+    
+def barplot(frame, col_x, col_y, country):
+    reduced  = frame.where(frame['country'] == country)
+    reduced = reduced.dropna()
+    x_axis = reduced[col_x]
+    y_axis = np.array(reduced[col_y])
+    print('Number of data: ',len(y_axis))
+    dict = {}
+    for ind , val in enumerate(x_axis):
+        if val not in dict:
+            dict[val] = 0
+            dict[val] += y_axis[ind]
+        else:
+            dict[val] += y_axis[ind]
+    
+    lists = list(dict.items())
+    x,y = zip(*lists)
+    x = list(x)
+    y = list(y)
+   
+
+    fig = plt.figure()
+    ax = fig.add_axes([0,0,1,1])
+    ax.bar(x,y)
+    plt.title('{} vs. {} in {}'.format(col_y, col_x, country))
+    plt.xlabel(col_x)
+    plt.ylabel(col_y)
+    plt.grid(True)
+    plt.show()
+    
+    
+def categorize(column):
+    dict = {}
+    key = int(1)
+    for i in np.unique(column):
+        if i not in dict:
+            dict[i] = key
+            key+=1
+    return dict
+
+    
+def suicide_gender_year(df):
+    
+    df2 = df[df['sex'] == 'male']
+    dict = {}
+    count =  np.array(df2['suicides_per_100k'])
+    for ind, y in enumerate(df2['year']):
+        if y not in dict:
+            dict[y] = 0
+            dict[y] += count[ind]
+        else:
+            dict[y] += count[ind]
+            
+    
+    lists = list(dict.items())
+    x,y = zip(*lists)
+    plt.bar(x,y, label='male')
+    
+    df2 = df[df['sex'] == 'female']
+    dict = {}
+    count =  np.array(df2['suicides_per_100k'])
+    for ind, y in enumerate(df2['year']):
+        if y not in dict:
+            dict[y] = 0
+            dict[y] += count[ind]
+        else:
+            dict[y] += count[ind]
+            
+    
+    lists = list(dict.items())
+    x,y = zip(*lists)
+    plt.bar(x,y, label='female')
+    
+    
+    plt.legend(loc='best')
+    
+    
+    plt.title('Gender Suicide Rate over different Years')
+    plt.xlabel('Year')
+    plt.ylabel('counts')
+    plt.grid(True)
+    plt.show()
+    
+def suicides_gdp_gender(df, country):
+    p1 = df[df['country'] == country]
+    pm = p1[p1['sex'] == 'male']
+    sui_num = []
+    gdp = []
+    for year in pm['year'].unique():
+        p2 = pm[pm['year'] == year]
+        sui_num.append(sum(p2.suicides_no))
+        gdp.append(np.average(p2['gdp']))
+        
+    x = []
+    y = []
+    for i,j in sorted(zip(gdp,sui_num)):
+        x.append(i)
+        y.append(j)
+
+    plt.scatter(x,y,label='male')
+    
+    pf = p1[p1['sex'] == 'female']
+    sui_num = []
+    gdp = []
+    for year in pf['year'].unique():
+        p2 = pf[pf['year'] == year]
+        sui_num.append(sum(p2.suicides_no))
+        gdp.append(np.average(p2['gdp']))
+        
+    x = []
+    y = []
+    for i,j in sorted(zip(gdp,sui_num)):
+        x.append(i)
+        y.append(j)
+    
+    plt.scatter(x,y,label='female')
+    
+    plt.legend(loc='best')
+    
+    
+    plt.title('{}'.format(country))
+    plt.xlabel('gdp')
+    plt.ylabel('suicide count')
+    plt.grid()
+    plt.show()
+    
+    
+def countries_suicide_rate(df):
+    rate=[]
+    countries=[]
+
+    for country in df['country'].unique():
+        countries.append(country)
+        rate.append(np.sum(df[df['country']==country].suicides_per_100k))
+        
+    fig = plt.figure(figsize=(150,6))
+    plt.bar(countries,rate)
+    plt.show()
+    
+
+    
+def suicides_age_year(df, age):
+    df2 = df[df['age'] == age]
+    dfm = df2[df2['sex']== 'male']
+    dict = {}
+    count =  np.array(dfm['suicides_per_100k'])
+    for ind, y in enumerate(dfm['year']):
+        if y not in dict:
+            dict[y] = 0
+            dict[y] += count[ind]
+        else:
+            dict[y] += count[ind]
+            
+    
+    lists = list(dict.items())
+    x,y = zip(*lists)
+    plt.bar(x,y, label='male')
+    
+    
+    
+    dff = df2[df2['sex']=='female']
+    
+    dict = {}
+    count =  np.array(dff['suicides_per_100k'])
+    for ind, y in enumerate(dff['year']):
+        if y not in dict:
+            dict[y] = 0
+            dict[y] += count[ind]
+        else:
+            dict[y] += count[ind]
+            
+    
+    lists = list(dict.items())
+    x,y = zip(*lists)
+    plt.bar(x,y,label='female')
+    
+    plt.legend(loc='best')
+
+    
+    plt.title('age: {}'.format(age))
+    plt.xlabel('year')
+    plt.ylabel('suicides/100k pop')
+    plt.grid()
+    plt.show()
+
+
+def suicides_per100k_age(df):
+    ages=[]
+    rate=[]
+    
+    for age in df['age'].unique():
+        ages.append(age)
+        rate.append(np.sum(df[df['age']==age].suicides_per_100k))
+        
+    
+    fig = plt.figure(figsize=(15,10))
+    plt.pie(rate, labels=ages, autopct='%1.1f%%')
+    plt.show()
+    
+def suicides_no_age(df):
+    ages=[]
+    rate=[]
+    
+    for age in df['age'].unique():
+        ages.append(age)
+        rate.append(np.sum(df[df['age']==age].suicides_no))
+        
+    
+    fig = plt.figure(figsize=(15,10))
+    plt.pie(rate, labels=ages, autopct='%1.1f%%')
+    plt.show()
+    
+    
+def suicide_by_age_pie(df):
+    aggregation_functions = {'suicides_no': 'sum'}
+    df_sex = df.groupby(df['sex']).aggregate(aggregation_functions)
+    plot = df_sex.plot.pie(subplots=True ,figsize=(5, 5),autopct='%1.1f%%',fontsize='17')
+    plt.ylabel("")
+    plt.title("Suicide percentages by sex",fontsize="17")
+    
+    
+    
+def suicide_by_year(df):
+    aggregation_functions = {'suicides_no': 'sum'}
+
+    df_yr_sex = df.groupby(['year', 'sex']).agg(aggregation_functions)
+    df_yr = df.groupby(df['year']).aggregate(aggregation_functions)
+    df_yr = df_yr.drop(2016)
+    df_yr.plot(kind="bar",rot = '50',figsize=(10,5),fontsize='14')
+    plt.title('Number of Suicides per Year',fontsize='14')
+    
+    
+def suicide_by_age_range(df):
+    aggregation_functions = {'suicides_no': 'sum'}
+
+    
+    df_age = df.groupby(df['age']).aggregate(aggregation_functions)
+    df_age = df_age.sort_index()
+    df_sex = df.groupby(df['sex']).aggregate(aggregation_functions)
+    df_multiple = df.groupby(['age', 'sex']).agg(aggregation_functions)
+    df_multiple = df_multiple.reset_index()
+    df_age = df_age.rename(index={'5-14 years': '05-14 years'})
+    df_age = df_age.sort_index()
+    
+    
+    list_men, list_women = [],[]
+    for i in range(len(df_multiple)):
+        if i%2:
+            list_men.append(df_multiple.values[i][2])
+        else:
+            list_women.append(df_multiple.values[i][2])
+
+    plotdata = pd.DataFrame({
+        "Men":list_men,
+        "Women":list_women,
+
+        }, 
+        index=df_age.index.tolist()
+    )
+
+    plotdata = plotdata.rename(index={'5-14 years': '05-14 years'})
+    plotdata = plotdata.rename(index={'05-14 years': '35-54 years','35-54 years': '05-14 years'})
+    plotdata = plotdata.sort_index()
+    plotdata.plot(kind="bar",rot=15,fontsize="14")
+    plt.title("Suicides by age and sex",fontsize='14')
+    plt.xlabel("Age",fontsize='14')
+    plt.ylabel("Number of Suicides",fontsize='14')
+    plt.show()
+
+def suicide_by_sex(df):
+    aggregation_functions = {'suicides_no': 'sum'}
+    df_sex = df.groupby(df['sex']).aggregate(aggregation_functions)
+    ax = df_sex.plot.bar(rot=0)
+    plt.title("Suicide Numbers by sex")
+    plt.show()
+    
+    
+    
+def suicide_by_age(df):
+    aggregation_functions = {'suicides_no': 'sum'}
+    df_age = df.groupby(df['age']).aggregate(aggregation_functions)
+    df_age = df_age.sort_index()
+    df_sex = df.groupby(df['sex']).aggregate(aggregation_functions)
+    df_multiple = df.groupby(['age', 'sex']).agg(aggregation_functions)
+    df_multiple = df_multiple.reset_index()
+    df_age = df_age.rename(index={'5-14 years': '05-14 years'})
+    df_age = df_age.sort_index()
+    ax = df_age.plot.bar(rot=15,fontsize='14')
+
+    plt.title("Suicides by Age",fontsize='14')
+    plt.xlabel("Age",fontsize='14')
+    plt.ylabel("Number of Suicides",fontsize='14')
+    plt.show()
+    
+
+
+def suicide_pearson_population(df):
+    
+    country_list=np.unique(df['country'])
+    year_list=list(np.unique(df['year']))
+
+    year_list.remove(1985)
+    year_list.remove(1986)
+    year_list.remove(2016)
+        
+    pear=[]
+    
+    for co in country_list:
+        
+        df_c = df[df['country'] == co]
+       
+        dff_facto=[]
+        dff_sui=[]
+        
+        for year in year_list:
+            
+            df_y = df_c[df_c['year'] == year]
+            df_y_s=list(df_y['suicides_no'])
+            n=sum(df_y_s)
+            
+            dff_sui.append(n)
+            
+            df_y_f=list(df_y['population'])
+            df_y_f_int=[]
+            
+            df_y_f_int=df_y_f
+                        
+            nn=sum(df_y_f_int)
+     
+            dff_facto.append(nn)            
+        
+        cor=np.corrcoef(dff_sui,dff_facto)
+        pear.append(cor[1][0])
+
+    return pear
+
+def Sui_by_pop(df, country):
+
+    df_c = df[df['country'] == country]
+    country_list=np.unique(df['country'])
+    year_list=list(np.unique(df['year']))
+
+    year_list.remove(1985)
+    year_list.remove(1986)
+    year_list.remove(2016)
+
+    dff_population=[]
+    dff_sui=[]
+
+    for year in year_list:
+
+        df_y = df_c[df_c['year'] == year]
+        df_y_s=list(df_y['suicides_no'])
+        n=sum(df_y_s)
+
+        dff_sui.append(n)
+
+        df_y_f=list(df_y['population'])
+        df_y_f_int=[]
+
+
+        df_y_f_int=df_y_f
+
+
+        nn=sum(df_y_f_int)
+
+        dff_population.append(nn)       
+
+    fig = plt.figure(figsize=(8,5))
+
+    plt.scatter(dff_population,dff_sui)
+    plt.title('{}'.format(country) ,Fontsize=25)
+    plt.tick_params(labelsize=15)   
+    plt.xticks(rotation=15);
+    plt.xlabel('Population',Fontsize=25)
+    plt.ylabel('num of suicide',Fontsize=25)
